219d5a64ef474c746868126939c184cf2c25bc0d
Maxime Maillet <maxime.maillet@eoko.fr>
2018-06-04 17:11:38 +0200

    infoconcert scrap

---
+++ src/scrappers/infoconcert.js
@@ -0,0 +1,120 @@
+const baseUrl = 'https://www.infoconcert.com';
+
+const Scrapper = require('../services/scrapper');
+const mongoService = require('../services/mongo');
+
+async function doSearch(name) {
+  try {
+    const scrapper = new Scrapper();
+    await scrapper.isExists(`${baseUrl}/recherche-concert.html?motclef=${name}`);
+
+    const scr = await scrapper.get();
+    const urls = {
+      artists: [],
+      events: [],
+      locations: [],
+    };
+
+    scr('a').each((index, value) => {
+      const link = scr(value).attr('href');
+      if(link.match(/^\/artiste.*\/concerts\.html$/)) {
+        urls.artists.push(baseUrl+link);
+      }
+
+      if(link.match(/^\/salle.*\/concerts\.html$/) || link.match(/^\/festival.*\/concerts\.html$/)) {
+        urls.events.push(baseUrl+link);
+      }
+    });
+
+    return urls;
+
+  } catch(e) {
+    throw e;
+  }
+}
+
+async function doArtist(url) {
+  try {
+    const toScrap = await mongoService.scrapFromArtist(url);
+    // console.log(`Artist (${url}) to scrap : ${toScrap}`);
+
+    if(toScrap) {
+      const scrapper = new Scrapper();
+      await scrapper.isExists(url);
+      // @todo
+      // mongoService.addScrap('artist', url);
+
+      const $ = await scrapper.get();
+      const dataScrap = {
+        artist: {},
+        events: [],
+      };
+
+      dataScrap.artist.name = $('h1[itemprop=name]').text();
+      dataScrap.artist.type = $('.single-intro-top .genre').find('a').text();
+      dataScrap.artist.popularity = $('span[itemprop=ratingCount]').text() | 0;
+      dataScrap.artist.logo = $('.single-intro .affiche').find('img').attr('src');
+
+      // @todo : pas assez d'infos pour le moment
+      // $('.date-line').each((index, value) => {
+      //   dataScrap.events.push({
+      //     name: $(value).find('span[itemprop=name]').text(),
+      //     startDate: $(value).find('time').attr('datetime'),
+      //     url: '',
+      //     location: {
+      //       name: $(value).find('span[itemprop=name]').text(),
+      //       city: $(value).find('span[itemprop=locality]').text(),
+      //     }
+      //   });
+      // });
+
+      return dataScrap;
+    }
+  } catch(e) {
+    throw e;
+  }
+}
+
+async function doEvent(url) {
+  const scrapper = new Scrapper();
+  await scrapper.isExists(url);
+
+  const $ = await scrapper.get();
+  const cpRegex = /(([0-8][0-9])|(9[0-5]))[0-9]{3}/;
+  const events = [];
+  const dataScrap = {
+    url: baseUrl+url,
+    location: {
+      name: $('h1[itemprop=name]').text(),
+      address: $('.single-intro .adr .street-address').text(),
+      postal_code: $('.single-intro .adr').text().match(cpRegex)[0],
+      city: $('.single-intro .adr .locality').text(),
+      country: '',
+      latitude: $('.geo .latitude .value-title').attr('title'),
+      longitude: $('.geo .longitude .value-title').attr('title'),
+    }
+  };
+
+  $('.date-line').each((index, value) => {
+    const artists = [];
+    $(value).find('.spectacle a').each((index, value) => {
+      artists.push($(value).text().toLowerCase());
+    });
+    events.push({
+      name: $(value).find('.festival-associe a').text() ? $('.festival-associe a').text() : $('h1[itemprop=name]').text(),
+      url: dataScrap.url,
+      location: dataScrap.location,
+      startDate: $(value).find('time').attr('datetime'),
+      artists: artists,
+    });
+  });
+
+  return events;
+}
+
+module.exports = {
+  baseUrl,
+  doSearch,
+  doArtist,
+  doEvent,
+};
--- src/services/scrapper.js
+++ src/services/scrapper.js
@@ -14,7 +14,7 @@ module.exports = function() {
           self.cache_html = html;
           resolve();
         } else {
-          reject(error)
+          reject(error);
         }
       });
     });
